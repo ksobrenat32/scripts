099a97238886d37d8a973745cce991d71b74ea8d
ksobrenat32 <ksobrenatural@tuta.io>
2021-06-22 10:46:00 -0500

    fix permission

--- install_config/podman.sh
+++ install_config/podman.sh
@@ -6,7 +6,7 @@ set -e
 __username=$(whoami)
 
 # Enable ping on non root containers
-sudo echo 'net.ipv4.ping_group_range=0 165535' >> /etc/sysctl.d/podman-ping.conf
+echo -e 'net.ipv4.ping_group_range=0 165535' | sudo tee /etc/sysctl.d/podman-ping.conf
 
 # Add podman registrie
 echo -e "[registries.search]\nregistries = ['docker.io']" | tee $HOME/.config/containers/registries.conf
